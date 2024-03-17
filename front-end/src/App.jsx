5c47b74a95b38de231a39c70e9d48ed6b4711977
Alberto Lucas da Trindade Neto <alberto.trindade@ufba.br>
2024-03-17 08:53:06 -0300

    Getting some info from the api

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from 'react';
+import { Grid, GridColumn as Column } from '@progress/kendo-react-grid';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [pokemon, setPokemon] = useState([]);
+  const [filter, setFilter] = useState({});
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=100')
+      .then(response => {
+        const promises = response.data.results.map(pokemon => 
+          axios.get(pokemon.url)
+        );
+        Promise.all(promises).then(pokemonResponses => 
+          setPokemon(pokemonResponses.map(response => response.data))
+        );
+      });
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Grid
+      data={pokemon.filter(p => 
+        String(p.name).toLowerCase().includes(filter.name || '')
+      )}
+    >
+      console.log(pokemon)
+      <Column field="id" title="ID" />
+      <Column field="sprites" title="Image" cell={(props) =>
+        <img src={props.dataItem.sprites.front_default} alt={props.dataItem.name} />
+      } />
+      <Column field="name" title="Name" />
+      <Column field="height" title="Height" />
+      <Column field="weight" title="Weight" />
+      <Column field="types" title="Types" cell={(props) => 
+        props.dataItem.types.map(type => type.type.name).join(', ')
+      } />
+      <Column field="abilities" title="Abilities" cell={(props) =>
+        props.dataItem.abilities.map(ability => ability.ability.name).join(', ')
+      } />
 
-export default App
+    </Grid>
+  );
+};
+
+export default App;
